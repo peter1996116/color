72dfbf89819fc8ebbfaa3650cb7530124b62ea45
peter <peter1996116@gmail.com>
2019-06-12 01:56:29 +0800

    color

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var redslide: UISlider!
+    @IBOutlet weak var blueslide: UISlider!
+    
+    @IBOutlet weak var greenslide: UISlider!
+    func onchanges(){
+        image.backgroundColor = UIColor(red:CGFloat( redslide.value), green: CGFloat(greenslide.value), blue: CGFloat(blueslide.value), alpha: CGFloat(1))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        image.backgroundColor = UIColor(red:CGFloat( redslide.value), green: CGFloat(greenslide.value), blue: CGFloat(blueslide.value), alpha: CGFloat(1))
+        
     }
-
-
+    
+    @IBAction func red(_ sender: Any) {
+        onchanges()
+    }
+    
+    @IBAction func green(_ sender: Any) {
+        onchanges()
+    }
+    
+    @IBAction func blue(_ sender: Any) {
+        onchanges()
+    }
 }
 
